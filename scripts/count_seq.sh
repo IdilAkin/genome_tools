dd7ea18c3b6ffff54051af74b43cb2c4b366538e
IdilAkin <48624989+IdilAkin@users.noreply.github.com>
2019-03-17 11:59:24 -0400

    lots of change

--- scripts/count_seq.sh
+++ scripts/count_seq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# USAGE: bash count_seq.sh FASTA - a bit change
+# USAGE: bash count_seq.sh FASTA lots of change
 # DESCRIPTION: script to count number of seqs in a genome
 
 grep -c ">" $1
